dc98c724b4e9e11317be2df488d7aac16fb0a140
Babu Natarajan <babunatarajan6@gmail.com>
2022-02-16 13:48:37 -0500

    Update ubuntu-setup.sh

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -2,7 +2,7 @@
 
 apt-get -y update
 apt-get install -y software-properties-common
-add-apt-repository -y ppa:certbot/certbot
+# add-apt-repository -y ppa:certbot/certbot
 apt-get -y update
 apt-get -y install python3-certbot-nginx nginx
 echo "certbot --nginx -d example.com -d www.example.com"
